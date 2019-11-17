19fb46be93554c75c6e7754e4e1e6be8e2b9155f
lyjacky11 <lyjacky@live.com>
2019-11-17 12:38:26 -0500

    Added second page

--- resources/views/summary.blade.php
+++ resources/views/summary.blade.php
@@ -66,36 +66,15 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
-                <div id="title" class="title m-b-md">
+                <div id="summary" class="title m-b-md">Summary
             </div>
-            <div id="clock" class="m-b-md"></div>
+            <div>
+            What is Material UI?
+            </div>
             <br>
-            <div id="button" class="m-b-md"></div>
-            <br>
-            <div class="links"><a href="{{ url('/summary') }}">Go To Summary Page</a></div>
-            <br>
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            <br>
+            <div>
+            What is Laravel?
             </div>
         </div>
     </body>
