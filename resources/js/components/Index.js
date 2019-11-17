589a020682f5ed308f6bf8bc508a4c8d962c62fa
lyjacky11 <lyjacky@live.com>
2019-11-16 21:40:37 -0500

    Added ReactJS and Material UI

---
+++ resources/js/components/Index.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+import logo from './logo.png';
+
+function CreateButton() {
+    return (
+      <Button variant="contained" color="primary">
+        I am a Material UI Button!
+      </Button>
+    );
+  }
+
+export default class Title extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <div className="card">
+                            <div className="card-header">Welcome to React JS!</div>
+                            <div className="card-body">I'm a React component! =)</div>
+                            <br></br>
+                            <div className="card-body"><img src={logo} alt="Logo" height="200"/></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class Clock extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        time: new Date().toLocaleString("en-US")
+      };
+    }
+    componentDidMount() {
+      this.intervalID = setInterval(
+        () => this.tick(),
+        500
+      );
+    }
+    componentWillUnmount() {
+      clearInterval(this.intervalID);
+    }
+    tick() {
+      this.setState({
+        time: new Date().toLocaleString("en-US")
+      });
+    }
+    render() {
+      return (
+        <h3 className="App-clock">
+          Current Time: {this.state.time}
+        </h3>
+      );
+    }
+  }
+
+ReactDOM.render(<Title/>, document.getElementById('title'));
+ReactDOM.render(<CreateButton/>, document.querySelector('#button'));
+ReactDOM.render(<Clock/>, document.getElementById('clock'));
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,19 +2,20 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"/>
 
-        <title>Laravel</title>
+        <title>CPS 530 Project | Fall 2019</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
 
         <!-- Styles -->
         <style>
             html, body {
                 background-color: #fff;
                 color: #636b6f;
-                font-family: 'Nunito', sans-serif;
+                font-family: 'Roboto', sans-serif;
                 font-weight: 200;
                 height: 100vh;
                 margin: 0;
@@ -80,21 +81,21 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
+                <div id="title" class="title m-b-md">
+            </div>
+            <div id="clock" class="m-b-md"></div>
+            <br>
+            <div id="button" class="m-b-md"></div>
+            <br>
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
+            <br>
             </div>
         </div>
     </body>
+    <script type="text/javascript" src="js/app.js"></script>
 </html>
