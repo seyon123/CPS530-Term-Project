253830fabcf90eac9a98f630f9b74d824b98b2d0
lyjacky11 <lyjacky@live.com>
2019-11-24 15:49:34 -0500

    Revert "Added loading icon before React renders"
    This reverts commit 01fd38e5117ed86da5c31b739f6d1ac72df77375.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,9 +8,7 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
         <!-- Styles -->
-        <link rel="stylesheet" type="text/css" href="css/center-radar.css"/>
         <link rel="stylesheet" type="text/css" href="css/style.css"/>
-        <script type="text/javascript" src="js/pace.min.js"></script>
     </head>
     <body>
         <div id="navigation">
