e5133c6a0bb77e4b1c817b667e5d2ad8b7382a57
lyjacky11 <lyjacky@live.com>
2019-11-26 12:38:06 -0500

    Changed content of home

--- resources/js/components/Grid.js
+++ resources/js/components/Grid.js
@@ -34,10 +34,10 @@ export default function FullWidthGrid() {
     <div className={classes.root}>
       <Grid container spacing={3}>
       <Grid item xs={6}>
-          <Paper className={classes.title}>Material UI</Paper>
+          <Paper className={classes.title}>MATERIAL UI</Paper>
         </Grid>
         <Grid item xs={6}>
-          <Paper className={classes.title}>Laravel</Paper>
+          <Paper className={classes.title}>LARAVEL</Paper>
         </Grid>
         <Grid item xs={12}>
           <Paper className={classes.title}>Popularity</Paper>
@@ -63,9 +63,10 @@ export default function FullWidthGrid() {
         <Grid item xs={6}>
           <Paper className={classes.paper}>
           <ul>
-          <li>It can handle complex web applications securely, at a considerably faster pace than other frameworks.</li>
-          <li>Laravel is suitable when developing applications with complex backend requirements, whether small or large.</li>
-          <li>Artisan– Developing command line apps can be a breeze. It provides an outstanding Artisan CLI that comprises of advanced tools which enable you to do tasks and migrations.</li>
+          <li>Can handle complex web applications securely, at a considerably faster pace than other frameworks.</li>
+          <li>Suitable when developing applications with complex backend requirements, whether small or large.</li>
+          <li>Artisan – Developing command line apps can be a breeze.</li>
+          <li>Provides an outstanding Artisan CLI that comprises of advanced tools which enable you to do tasks and migrations.</li>
           </ul>
           </Paper>
         </Grid>
@@ -94,10 +95,10 @@ export default function FullWidthGrid() {
           <Paper className={classes.title}>Examples and Use Cases</Paper>
         </Grid>
         <Grid item xs={6}>
-          <Paper className={classes.paper}>This framework is currently used by Walmart, Bethesda, NASA and shutterstock. It is ideal for websites that provide an easy to use, clean and attractive user interface.</Paper>
+          <Paper className={classes.paper}>Currently used by Walmart, Bethesda, NASA and shutterstock. Ideal for websites that provide an easy to use, clean and attractive user interface.</Paper>
         </Grid>
         <Grid item xs={6}>
-          <Paper className={classes.paper}>Laravel is currently used to power startups.co, Fox47, and various other sites. This framework is ideal for developers who want to build B2B or enterprise websites that will evolve with changing web trends.</Paper>
+          <Paper className={classes.paper}>Currently used to power startups.co, Fox47, and various other sites. Ideal for developers who want to build B2B or enterprise websites that will evolve with changing web trends.</Paper>
         </Grid>
       </Grid>
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,19 +23,6 @@
             </div>
         </div>
         <div class="position-ref full-height">
-            <!-- @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif -->
             <div class="content">
                 <div id="title" class="title m-b-md"></div>
                 <div class="text m-b-md"><h4>Created By:</h4></div>
@@ -50,71 +37,6 @@
                 <br><br>
                 <br>
                 <div id="grid"></div>
-                <!-- <table class="snapshot" id="frontEnd">
-                    <tr>
-                    <th colspan="2"><h2>Front-end Framework: Material UI</h2></th>
-                    </tr>
-                    <tr>  
-                        <th>Popularity</th>
-                        <td>Material-UI is the most popular React UI component library.</td>
-                    </tr>
-                    <tr>
-                        <th rowspan="3">Strengths</th>
-                        <td>Scalable, you can build a wide range of products.</td>
-
-                        <tr>
-                            <td>A smooth, easy and appealing interface.</td>
-                        </tr>
-                        <tr>
-                            <td>Well documented - makes it a beginner friendly front-end.</td>
-                        </tr>
-                    <tr>
-                        <th rowspan="3">Weaknesses</th>
-                        <td>Not great for heavy-data apps - Poor number of Data-originated Components</td>
-
-                        <tr>
-                            <td> It can be hard to discern interactive elements.</td>
-                        </tr>
-                        <tr>
-                            <td>Not platform independent -Affiliation with Google ecosystem.</td>
-                        </tr>
-                    <tr>
-                        <th>Examples and Use Cases</th>
-                        <td>This frame work is currently used by Walmart, Bethesda, NASA and shutterstock. It is ideal for websites that provide an easy to use, clean and attractive user interface.</td>
-                    </tr>
-                </table>
-                <table class="snapshot" id="backEnd">
-                    <tr>
-                        <th colspan="2"><h2>Back-end Framework: Laravel</h2></th>
-                    </tr>
-                    <tr>  
-                        <th>Popularity</th>
-                        <td>Introduced in 2011, Laravel has become the most popular free, open-source PHP framework.</td>
-                    </tr>
-                    <tr>
-                        <th rowspan="3">Strengths</th>
-                        <td>It can handle complex web applications securely, at a considerably faster pace than other frameworks.</td>
-                    </tr>
-                            <tr>
-                                <td>Laravel is suitable when developing applications with complex backend requirements, whether small or large.</td>
-                            </tr>
-                            <tr>
-                                <td>Artisan– Developing command line apps can be a breeze. It provides an outstanding Artisan CLI  that comprises of advanced tools which enable you to do tasks and migrations.</td>
-                    <tr>
-                        <th rowspan="3">Weaknesses</th>
-                        <td>To modify core behavior there will be a lack of options.</td>
-
-                            <tr>
-                                <td>Speed and performance will be affected.</td>
-                            </tr>
-                            <tr>
-                                <td>Community support is not widespread compared to other platforms.</td>
-                            </tr>
-                    <tr>
-                        <th>Examples and Use Cases</th>
-                        <td>Laravel is currently used to power startups.co, Fox47, and various other sites. This framework is ideal for developers who want to build B2B or enterprise websites that will evolve with changing web trends.</td>
-                    </tr>
-                </table> -->
                 <br><br>
             </div>
         </div>
