45836c0ed13449b2f02c99308efc144b1ba57c07
lyjacky11 <lyjacky@live.com>
2019-11-30 22:34:40 -0500

    Added conclusion template

--- resources/js/components/Conclusion.js
+++ resources/js/components/Conclusion.js
@@ -11,21 +11,56 @@ export default class Conclusion extends Component {
         <div className="row justify-content-center">
           <div className="col-md-8">
             <div className="card">
-              <div className="card-header">Conclusion</div>
+              <div className="card-header" style={{fontSize: '3rem'}}>Conclusion</div>
               <br></br>
-              <div className="card-body"><img src={logo} alt="Logo" height="200" /></div>
+              <div className="card-body">Framework Expectations</div>
               <br></br>
-              <div><p style={{ fontSize: '16px', margin: 'auto 20%' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              <div className="card-body">
+              <p style={{ fontSize: '16px', margin: 'auto 20%' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               In nec leo sit amet sem faucibus pulvinar. In nibh mi, viverra eget nisi nec, tempor varius magna.
               Quisque ipsum ligula, ullamcorper at urna ac, sollicitudin efficitur lacus. Aenean varius mollis sapien, ac ultrices ex dapibus viverra.
-              </p></div>
+              </p>
+              </div>
               <br></br>
-              <div><p style={{ fontSize: '16px', margin: 'auto 20%' }}>Aliquam sed urna a leo molestie volutpat.
+              <div className="card-body">Install & Configuration</div>
+              <br></br>
+              <div className="card-body"><p style={{ fontSize: '16px', margin: 'auto 20%' }}>Aliquam sed urna a leo molestie volutpat.
               Vestibulum egestas congue leo, ac aliquet tellus tincidunt non.
               Etiam pretium, lacus non tempus condimentum, dolor ipsum convallis tortor, quis fermentum tellus ante in ligula.
               Curabitur feugiat lectus vitae ipsum aliquam, ac ultricies ipsum suscipit.
               Interdum et malesuada fames ac ante ipsum primis in faucibus.
-              </p></div>
+              </p>
+              </div>
+              <br></br>
+              <div className="card-body">Creating Pages</div>
+              <br></br>
+              <div className="card-body"><p style={{ fontSize: '16px', margin: 'auto 20%' }}>Aliquam sed urna a leo molestie volutpat.
+              Vestibulum egestas congue leo, ac aliquet tellus tincidunt non.
+              Etiam pretium, lacus non tempus condimentum, dolor ipsum convallis tortor, quis fermentum tellus ante in ligula.
+              Curabitur feugiat lectus vitae ipsum aliquam, ac ultricies ipsum suscipit.
+              Interdum et malesuada fames ac ante ipsum primis in faucibus.
+              </p>
+              </div>
+              <br></br>
+              <div className="card-body">Learning Curve</div>
+              <br></br>
+              <div className="card-body"><p style={{ fontSize: '16px', margin: 'auto 20%' }}>Aliquam sed urna a leo molestie volutpat.
+              Vestibulum egestas congue leo, ac aliquet tellus tincidunt non.
+              Etiam pretium, lacus non tempus condimentum, dolor ipsum convallis tortor, quis fermentum tellus ante in ligula.
+              Curabitur feugiat lectus vitae ipsum aliquam, ac ultricies ipsum suscipit.
+              Interdum et malesuada fames ac ante ipsum primis in faucibus.
+              </p>
+              </div>
+              <br></br>
+              <div className="card-body">Regrets of Frameworks</div>
+              <br></br>
+              <div className="card-body"><p style={{ fontSize: '16px', margin: 'auto 20%' }}>Aliquam sed urna a leo molestie volutpat.
+              Vestibulum egestas congue leo, ac aliquet tellus tincidunt non.
+              Etiam pretium, lacus non tempus condimentum, dolor ipsum convallis tortor, quis fermentum tellus ante in ligula.
+              Curabitur feugiat lectus vitae ipsum aliquam, ac ultricies ipsum suscipit.
+              Interdum et malesuada fames ac ante ipsum primis in faucibus.
+              </p>
+              </div>
             </div>
           </div>
         </div>
